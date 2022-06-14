36008abb865f38214f22a23b912ab16fe039b87f
Maximiliano Moretta <maximoretta7@gmail.com>
2022-06-13 23:06:04 -0300

    simulador con arrays y objetos

--- miarchivo.js
+++ miarchivo.js
@@ -1,98 +1,92 @@
 
+const formas = ["perita", "cilindro","pirámide", "gota"
+]
+const tipos = ["frase", "cactus", "flores", "personalizado"
+]
 
-function iniciar(){
+const precios = {
+   formas: {
+        perita: 1000,
+        cilindro: 2000,
+        pirámide: 2500,
+        gota: 3000,
+    },
+   
+    tipos: {
+        frase: 1500,
+        cactus: 1800,
+        flores: 2000,
+        personalizado: 3000
+    }
+}
+
+class Mate { 
+    constructor( a, b, c){
+        this.forma = formas[a]
+        this.tipo = tipos[b]
+        this.cantidad = c
+        this.precio = 0
+    }
+    información(){
+        alert("Tu mate elgido fue un: " + this.forma + " con " + this.tipo + " , y te llevas " + this.cantidad + " unidades")
+    }
+    calcularPrecio(){
+        this.precio = (precios.formas[this.forma] + precios.tipos[this.tipo]) * this.cantidad
+        alert("Estas a punto de pagar: $" + this.precio)
+    }
+}
+
+function crearMate(){
     let forma = 0
-    while (forma <1 || forma >3){
-        forma = Number(prompt("Bienvenido, elige el tipo de mate que deseas\n 1-mate perita \n 2-mate cilindro \n 3-mate pirmide"))
+    while (forma <1 || forma >4){
+        forma = Number(prompt("Bienvenido, elige el tipo de mate que deseas\n 1-mate perita \n 2-mate cilindro \n 3-mate pirámide \n 4-mate gota"))
     } 
-    
-    let nombreForma = obtenerForma(forma)
-    alert("Buenisimo, has elegido el: " + nombreForma)
+    forma = forma-1
     
     let tipo = 0
-    while (tipo <1 || tipo >3){
-        tipo = Number(prompt("Ahora elije el tipo de mate que te gusta: \n 1-con frase \n 2-con cactus \n 3-personalizado"))
+    while (tipo <1 || tipo >4){
+        tipo = Number(prompt("Ahora elije el tipo de mate que te gusta: \n 1-con frase \n 2-con cactus \n 3- con flores \n 4-personalizado"))
     }   
+    tipo = tipo-1
 
-    let tipoNombre = obtenerTipo(tipo)
-    alert("Que buena eleccion la del mate: " + tipoNombre)
-    let precioNumero = obtenerPrecio(tipo,forma)
-    alert("Tu mate elegido es un :" + nombreForma + " " + tipoNombre + " y cuesta $" + precioNumero)
-    
     let cantidad = Number(prompt("Cuantos deseas comprar?"))
-    alert("Perfecto, tu pedido se encuentra en el carrito")
-    
-    console.log("Estás a punto de comprar: " +  cantidad + " unidades de" + " " + nombreForma + " " + tipoNombre)
-    console.log("Total $" + obtenerTotal(precioNumero, cantidad))
+
+    const miMate = new Mate(forma, tipo, cantidad)
+
+    miMate.información()
+    miMate.calcularPrecio()
+
+    return miMate
 }
 
 function obtenerTotal(precioUnitario, cantidad){
     return precioUnitario*cantidad
 }
 
+function iniciar(){
+    let precioTotal= 0
 
-function obtenerForma(forma){
-    if ( forma === 1){
-        return "Mate Perita"
-    }
-    else if ( forma === 2){
-        return "Mate Cilindro"
-    }
-    else if( forma === 3){
-        return "Mate Piramide"
-    }
-    else {
-        throw "No es un numero de forma valido"
-    }
-}
+    carrito = [
 
-function obtenerTipo(tipo){
-    if ( tipo === 1){
-        return "Con Frase"
-    }
-    else if ( tipo === 2){
-        return "Con Cactus"
-    }
-    else if( tipo === 3){
-        return "Personalizado"
-    }
-    else {
-        throw "No es un numero de tipo valido"
-    }
+    ]
+ 
+    let continuar = true
 
-}
+    while(continuar === true){
+       let mate = crearMate()
+       carrito.push(mate)
+       let avance = prompt("Deseas seguir comprando?")
+            if( avance == "no"){
+                continuar = false
+            }
+    }
+    alert("Usted está por comprar: " + carrito.length + " modelos de mates")
 
-function obtenerPrecio(tipo, forma){
-
-    let precioForma
-    let precioTipo
-
-    if ( forma === 1){
-       precioForma = 1500
+    for (let i = 0; i < carrito.length; i++) {
+        const matecompra = carrito[i]
+        precioTotal = precioTotal + matecompra.precio
     }
-    else if ( forma === 2){
-        precioForma = 2000
-    }
-    else if( forma === 3){
-       precioForma = 2500
-    }
-    else {
-        throw "No es un numero de tipo valido"
-    }
-
-    if ( tipo === 1){
-        precioTipo = 1000
-     }
-     else if ( tipo === 2){
-         precioTipo = 2000
-     }
-     else if( tipo === 3){
-        precioTipo = 1500 
-    }
-     else {
-         throw "No es un numero de tipo valido"
-     }
-     return precioForma + precioTipo
+    alert("El valor total a abonar es de $ " + precioTotal)
 }
 
 iniciar()
