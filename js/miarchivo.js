8988f719adff066cc81ca408deb2fc8c95ee3a94
Maximiliano Moretta <maximoretta7@gmail.com>
2022-07-13 22:53:30 -0300

    Use Fetch for read Json

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,82 +1,85 @@
-const carrito = []
+         
+    const carrito = []
+    
+    const formas = ["perita", "cilindro","pirámide", "gota"]
+    
+    const tipos = ["frase", "cactus", "flores", "personalizado"]
+    
+    const precioTotal = document.getElementById("precioTotal")
 
-const formas = ["perita", "cilindro","pirámide", "gota"]
 
-const tipos = ["frase", "cactus", "flores", "personalizado"]
+function getData(url, cb) {
+    fetch(url)
+    .then(response => response.json())
+    .then(result => cb(result));
+    }
+  let url = "https://raw.githubusercontent.com/MaxiMoretta/Coderhouse_Javascript/main/js/mates.json"    
+getData(url, render)
 
-const precioTotal = document.getElementById("precioTotal")
+function render(data){
 
-const listaformas = document.getElementById("listaformas")
+    const listaMates = data.listaMates
+    const listaTipos = data.listaTipos
+    
+    console.log(listaMates)
+    console.log(listaTipos)
+    console.log(data)
+    
 
-/* const obtenerLista = () => {
-    let listas = {}
-    fetch("https://raw.githubusercontent.com/MaxiMoretta/Coderhouse_Javascript/main/js/mates.json"
-    ).then(response => response.json()
-    ).then(function(data){
-        listas = data
+    
+    const listaformas = document.getElementById("listaformas")
+    
+       
+    listaMates.forEach((producto) => {
+        const div = document.createElement('div')
+        div.classList.add("col-sm-3")
+        div.innerHTML = `<h4>${producto.nombre}</h4>
+                        <img src="img/${producto.img}" class="col-formas" value="${producto.nombre}"/>
+                        <p>Precio: $ ${producto.precio}</p>
+                        <hr>
+                        `   
+        listaformas.append(div)
     })
-
-    return listas
-} */
-
-async function getData(url) {
-    const response = await fetch(url);
-  
-    return response.json();
-  }
-  const data = await getData("https://raw.githubusercontent.com/MaxiMoretta/Coderhouse_Javascript/main/js/mates.json");
-
-  const listaMates = data.listaMates
-  const listaTipos = data.listaTipos
-
-listaMates.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add("col-sm-3")
-    div.innerHTML = `<h4>${producto.nombre}</h4>
-                    <img src="img/${producto.img}" class="col-formas" value="${producto.nombre}"/>
-                    <p>Precio: $ ${producto.precio}</p>
-                    <hr>
-                    `   
-    listaformas.append(div)
-})
-
-btnsFormas= document.getElementsByClassName("col-formas") 
-
-for (let index = 0; index < btnsFormas.length; index++) {
-    const btn = btnsFormas[index]
-
-    btn.addEventListener('click', (event) => {
-        const value = event.target.getAttribute("value")
-        console.log(value)
-        const campoforma = document.getElementById("formForma")
-        campoforma.setAttribute("value", value)
-    })  
-}
-
-const listatiposcontainer = document.getElementById("listatipos")
-
-listaTipos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add("col-sm-3")
-    div.innerHTML = `<h4>${producto.nombre}</h4>
-                    <img src="img/${producto.img}" class="col-tipos" value="${producto.nombre}"/>
-                    <p>Precio: $ ${producto.precio}</p>
-                    <hr>
-                    `   
-    listatiposcontainer.append(div)
-})
-
-btnsTipos= document.getElementsByClassName("col-tipos") 
-
-for (let index = 0; index < btnsTipos.length; index++) {
-    const btn = btnsTipos[index]
-
-    btn.addEventListener('click', (event) => {
-        const value = event.target.getAttribute("value")
-        console.log(value)
-        const campoforma = document.getElementById("formTipo")
-        campoforma.setAttribute("value", value)
-    })  
+    
+    btnsFormas= document.getElementsByClassName("col-formas") 
+    
+    for (let index = 0; index < btnsFormas.length; index++) {
+        const btn = btnsFormas[index]
+    
+        btn.addEventListener('click', (event) => {
+            const value = event.target.getAttribute("value")
+            console.log(value)
+            const campoforma = document.getElementById("formForma")
+            campoforma.setAttribute("value", value)
+        })  
+    }
+    
+    const listatiposcontainer = document.getElementById("listatipos")
+    
+    listaTipos.forEach((producto) => {
+        const div = document.createElement('div')
+        div.classList.add("col-sm-3")
+        div.innerHTML = `<h4>${producto.nombre}</h4>
+                        <img src="img/${producto.img}" class="col-tipos" value="${producto.nombre}"/>
+                        <p>Precio: $ ${producto.precio}</p>
+                        <hr>
+                        `   
+        listatiposcontainer.append(div)
+    })
+    
+    btnsTipos= document.getElementsByClassName("col-tipos") 
+    
+    for (let index = 0; index < btnsTipos.length; index++) {
+        const btn = btnsTipos[index]
+    
+        btn.addEventListener('click', (event) => {
+            const value = event.target.getAttribute("value")
+            console.log(value)
+            const campoforma = document.getElementById("formTipo")
+            campoforma.setAttribute("value", value)
+        })  
+    }
+    
 }
 
 const precios = {
